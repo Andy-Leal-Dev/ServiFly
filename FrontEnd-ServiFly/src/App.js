591d4267d24c63fff11c551fc28ffef6d42b4a13
iamXavii <oscarxavier.santander@gmail.com>
2025-06-13 23:34:27 -0400

    Add files via upload
    Listo la vista del Login, con responsive y el favicon

---
+++ FrontEnd-ServiFly/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Login from './components/Login'; // Ajusta la ruta según dónde pongas Login.jsx
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
