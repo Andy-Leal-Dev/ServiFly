1630d5c01b3aafb83470de21fbe946c187776cc6
Andrierber Leal <lealandrierber343@gmail.com>
2025-06-14 16:14:42 -0400

    V-0.0.1.0 (Añadir página de inicio de sesión y enrutamiento, implementar página de inicio y crear estilos globales.
    - Se introdujo una nueva página de inicio de sesión con elementos de formulario para la autenticación de usuarios.
    - Se creó una página de inicio con un encabezado simple.
    - Se configuró el enrutamiento de la aplicación con React Router.
    - Se añadieron estilos globales a la aplicación, incluyendo un diseño responsivo para la página de inicio de sesión.
    - Se incluyeron estilos para los componentes de navegación e inicio de sesión, mejorando la interfaz de usuario general.
    - Se añadió el logotipo SVG para React y otros recursos para la marca.)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Link } from 'react-router-dom';
+import AppRoutes from '/src/routes/AppRoutes';
+import './styles/navGlobal.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <nav className='navGlobal'>
+        <Link to="/">Inicio</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
